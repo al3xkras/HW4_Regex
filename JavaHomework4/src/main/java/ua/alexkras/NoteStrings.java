f2c9c3a90980af00caf769c851bc332ee472c7c7
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-09-01 17:00:43 +0300

    Add interface NoteStrings & edit properties

---
+++ JavaHomework4/src/main/java/ua/alexkras/NoteStrings.java
@@ -0,0 +1,29 @@
+package ua.alexkras;
+
+public interface NoteStrings {
+     String surname = "regex.surname";
+     String name = "regex.name";
+     String patronymic = "regex.patronymic";
+     String fullName = "regex.fullName";
+
+     String nickName = "regex.nickName";
+
+     String comment = "regex.comment";
+
+     String group = "regex.group";
+
+     String homePhoneNumber = "regex.homePhoneNumber";
+     String personalPhoneNumber = "regex.personalPhoneNumber";
+     String personalPhoneNumber2 = "regex.personalPhoneNumber2";
+
+     String eMail = "regex.eMail";
+
+     String skypeAddress = "regex.skypeAddress";
+
+     String addressPostalIndex = "regex.addressPostalIndex";
+     String addressCity = "regex.addressCity";
+     String addressStreet = "regex.addressStreet";
+     String addressHouseNumber = "regex.addressHouseNumber";
+     String addressFlatNumber = "regex.addressFlatNumber";
+     String fullAddress = "regex.fullAddress";
+}
