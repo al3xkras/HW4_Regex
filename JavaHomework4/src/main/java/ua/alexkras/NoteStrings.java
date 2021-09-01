8adb5bbec27436e211b8b5285cb5cfa9bf5d6868
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-09-01 20:35:59 +0300

    Edit NoteStrings interface

--- JavaHomework4/src/main/java/ua/alexkras/NoteStrings.java
+++ JavaHomework4/src/main/java/ua/alexkras/NoteStrings.java
@@ -1,29 +1,25 @@
 package ua.alexkras;
 
 public interface NoteStrings {
-     String surname = "regex.surname";
-     String name = "regex.name";
-     String patronymic = "regex.patronymic";
-     String fullName = "regex.fullName";
+     String positionPrefix = "position.";
+     String regexPrefix = "regex.";
 
-     String nickName = "regex.nickName";
-
-     String comment = "regex.comment";
-
-     String group = "regex.group";
-
-     String homePhoneNumber = "regex.homePhoneNumber";
-     String personalPhoneNumber = "regex.personalPhoneNumber";
-     String personalPhoneNumber2 = "regex.personalPhoneNumber2";
-
-     String eMail = "regex.eMail";
-
-     String skypeNickName = "regex.skypeNickName";
-
-     String addressPostalIndex = "regex.addressPostalIndex";
-     String addressCity = "regex.addressCity";
-     String addressStreet = "regex.addressStreet";
-     String addressHouseNumber = "regex.addressHouseNumber";
-     String addressFlatNumber = "regex.addressFlatNumber";
-     String fullAddress = "regex.fullAddress";
+     String[] PositionNames = {
+             "surname",
+             "name",
+             "patronymic",
+             "nickName",
+             "comment",
+             "group",
+             "homePhoneNumber",
+             "personalPhoneNumber",
+             "personalPhoneNumber2",
+             "eMail",
+             "skypeNickName",
+             "addressPostalIndex",
+             "addressCity",
+             "addressStreet",
+             "addressHouseNumber",
+             "addressFlatNumber",
+     };
 }
