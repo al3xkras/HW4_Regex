53e3172f206c74bbabfb956013afb1fa018e8e17
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-09-02 09:50:52 +0300

    Merge RegexScanner with Controller

--- JavaHomework4/src/main/java/ua/alexkras/Controller.java
+++ JavaHomework4/src/main/java/ua/alexkras/Controller.java
@@ -1,4 +1,43 @@
 package ua.alexkras;
 
+
+import java.util.Scanner;
+
 public class Controller {
+    public Model model;
+    public View view;
+
+    public Controller(Model model, View view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public String inputRegexWithScanner(String regex){
+        String output;
+        Scanner scanner = new Scanner(System.in);
+        while (!(scanner.hasNext() && (output = scanner.next()).matches(regex))){
+            view.printInvalidDataFormat();
+        }
+        return output;
+    }
+
+    public void inputNewNote(){
+        model.newNote();
+
+        while (model.noteBookDataModel.hasNextPosition()){
+            view.printCurrentPositionMessage();
+            view.printCurrentPositionRegex();
+
+            String input = inputRegexWithScanner(model.noteBookDataModel.getCurrentRegex());
+
+            model.noteBookDataModel.nextPosition();
+
+            model.note.addPosition(model.noteBookDataModel.getCurrentPositionName(),input);
+
+        }
+
+        model.addNote();
+    }
+
+
 }
