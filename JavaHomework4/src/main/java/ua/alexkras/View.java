ca50d12a62257bd30576d90a9fef83263b2c2fdc
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-09-02 08:32:24 +0300

    Edit View & add new StringConstants.properties

--- JavaHomework4/src/main/java/ua/alexkras/View.java
+++ JavaHomework4/src/main/java/ua/alexkras/View.java
@@ -1,4 +1,43 @@
 package ua.alexkras;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class View {
+
+    private final String bundleRegexName = "NoteRegex";
+    private final String bundleValuesName = "NotePositionNames";
+    private final String bundleConstantsName = "StringConstants";
+
+    public ResourceBundle bundleValues = ResourceBundle.getBundle(
+            bundleValuesName,
+            new Locale("ua","UA"));
+
+    public ResourceBundle bundleRegex = ResourceBundle.getBundle(
+            bundleRegexName,
+            new Locale("ua","UA"));
+
+    public ResourceBundle bundleConstants = ResourceBundle.getBundle(
+            bundleConstantsName,
+            new Locale("ua","UA"));
+
+    public String errorMessage;
+
+    public NoteBookDataModel dataModel;
+
+    public View(){
+        dataModel = new NoteBookDataModel(bundleRegex,bundleValues);
+        errorMessage = bundleConstants.getString(NoteStrings.errorMessage);
+    }
+
+    public void printCurrentPositionMessage(){
+        String message = dataModel.getCurrentValue();
+        System.out.println(message);
+    }
+
+    public void printInvalidDataFormat(){
+        System.out.println(errorMessage);
+    }
+
+
 }
