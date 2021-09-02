a34f8ca2d68689deef3a78ea4d52d95bafb8c293
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-09-02 15:36:42 +0300

    Main: edit access modifiers & starting point

--- JavaHomework4/src/main/java/ua/alexkras/Main.java
+++ JavaHomework4/src/main/java/ua/alexkras/Main.java
@@ -1,7 +1,6 @@
 package ua.alexkras;
 
 
-import java.util.ArrayList;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -12,24 +11,25 @@ public class Main {
 
     public static final Locale locale = new Locale("ua","UA");
 
-    public static ResourceBundle bundleValues = ResourceBundle.getBundle(
-            bundleValuesName,
-            locale);
+    public static final ResourceBundle bundleValues = ResourceBundle.getBundle(
+            bundleValuesName, locale);
 
-    public static ResourceBundle bundleRegex = ResourceBundle.getBundle(
-            bundleRegexName,
-            locale);
+    public static final ResourceBundle bundleRegex = ResourceBundle.getBundle(
+            bundleRegexName, locale);
 
-    public static NoteBookDataModel noteBookDataModel = new NoteBookDataModel(bundleRegex,bundleValues);
-    public static NoteBook noteBook = new NoteBook();
+    public static final NoteBookDataModel noteBookDataModel = new NoteBookDataModel(bundleRegex,bundleValues);
+    public static final NoteBook noteBook = new NoteBook();
 
-    public static Model model = new Model(noteBook,noteBookDataModel);
+    public static final Model model = new Model(noteBook,noteBookDataModel);
 
-    public static View view= new View(noteBookDataModel,locale);
+    public static final View view= new View(noteBookDataModel,locale);
 
-    public static Controller controller = new Controller(model,view);
+    public static final Controller controller = new Controller(model,view);
 
     public static void main(String[] args) {
         controller.inputNewNote();
+
+        model.note.printNote();
+
     }
 }
